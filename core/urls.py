d3c4be06634c417c6608cf39c44eb769a81380be
leonegao <leonegaouk@gmail.com>
2023-12-21 17:34:30 +0000

    weblog

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path("settings", views.settings, name="settings"),
+    path('logout', views.logout, name="logout"),
+    path('upload', views.upload, name='upload'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('follow', views.follow, name='follow'),
+    path('like-post', views.like_post, name='like-post'),
+    path('search',views.search,name='search'),
+    path('delete_post/<uuid:uuid>/', views.delete_post, name='delete_post'),
+    path('videos',views.videos,name='videos'),
+
+
+]
