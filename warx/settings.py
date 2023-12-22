05d8fcc613b8885c3b645ac0665be8408c9a4eb8
Leonardo costa da Silva <leonegaouk@gmail.com>
2023-12-22 11:44:58 +0000

    Update settings.py

--- warx/settings.py
+++ warx/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5l@4u6cs3-pfdw3kf^%ttu%ax03+z%ps+-_d5f=5hb=-*k*uts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
